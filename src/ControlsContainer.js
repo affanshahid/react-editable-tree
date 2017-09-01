ec1b877c8befa88b86d3591e0cbd3d56f813eccb
Affan Shahid <affan.shahid.94@gmail.com>
2017-09-02 01:53:32 +0500

    Add hooks to controls

--- src/ControlsContainer.js
+++ src/ControlsContainer.js
@@ -1,82 +1,149 @@
 import React, { Component } from 'react';
+import { func } from 'prop-types';
 import { connect } from 'react-redux';
 import { addNode, removeNode } from './actions';
 import { MainControls, ConfirmControls } from './Controls';
 import './ControlsContainer.css';
 
+const confirmationModes = {
+    remove: "REMOVE_MODE",
+    edit: 'EDIT_MODE',
+    none: 'NONE'
+};
+
 class ControlsContainer extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            removeConfirmation: false
-        }
+            confirmationMode: confirmationModes.none
+        };
 
         this.handleCancel = this.handleCancel.bind(this);
+        this.handleCancelRemove = this.handleCancelRemove.bind(this);
+        this.handleCancelEdit = this.handleCancelEdit.bind(this);
+
         this.handleRemove = this.handleRemove.bind(this);
+        this.handleEdit = this.handleEdit.bind(this);
+
         this.handleClickOutside = this.handleClickOutside.bind(this);
+
+        this.handleConfirm = this.handleConfirm.bind(this);
     }
 
     handleClickOutside(ev) {
-        this.handleCancel(ev)
+        this.handleCancel(ev);
     }
 
-    handleCancel(ev) {
+    handleCancelEdit(ev) {
         this.setState({
-            removeConfirmation: false
+            confirmationMode: confirmationModes.none
+        });
+        this.props.onCancelEdit();
+        ev.stopPropagation();
+    }
+
+    handleEdit(ev) {
+        this.setState({
+            confirmationMode: confirmationModes.edit
+        });
+        this.props.onBeginEdit();
+        ev.stopPropagation();
+    }
+
+    handleCancelRemove(ev) {
+        this.setState({
+            removeConfirmation: confirmationModes.none
         });
         ev.stopPropagation();
     }
 
     handleRemove(ev) {
         this.setState({
-            removeConfirmation: true
+            confirmationMode: confirmationModes.remove
         });
         ev.stopPropagation();
     }
 
+    handleConfirm(ev) {
+        switch (this.state.confirmationMode) {
+            case confirmationModes.remove:
+                this.props.handleConfirmRemove(ev);
+                break;
+            case confirmationModes.edit:
+                this.props.handleConfirmEdit(ev);
+                this.setState({
+                    confirmationMode: confirmationModes.none
+                });
+                break;
+            default:
+                throw new Error("Unexpected state for ControlsContainer");
+                
+        }
+    }
+    
+    handleCancel(ev) {
+        switch (this.state.confirmationMode) {
+            case confirmationModes.remove:
+                this.handleCancelRemove(ev);
+                break;
+            case confirmationModes.edit:
+                this.handleCancelEdit(ev);
+                break;
+            default:
+                throw new Error("Unexpected state for ControlsContainer");
+        }
+    }
+
     render() {
-        const { handleAdd, handleConfirmRemove, handleEdit, visible } = this.props;
+        const { handleAdd, visible } = this.props;
         const mainControls = <MainControls
             visible={visible}
             onAdd={handleAdd}
             onRemove={this.handleRemove}
-            onEdit={handleEdit} />
+            onEdit={this.handleEdit} />
 
         const confirmRemoveControls = <ConfirmControls
-            disableOnClickOutside={!this.state.removeConfirmation}
+            disableOnClickOutside={this.state.confirmationMode === confirmationModes.none}
             handleClickOutside={this.handleClickOutside}
-            onConfirm={handleConfirmRemove}
+            onConfirm={this.handleConfirm}
             onCancel={this.handleCancel} />
 
-        return this.state.removeConfirmation ? confirmRemoveControls : mainControls;
+        return this.state.confirmationMode !== confirmationModes.none ? confirmRemoveControls : mainControls;
     }
 }
 
-function handleAdd(ev, dispatch, id) {
-    console.log('added to ' + id);
-    ev.stopPropagation();
-    dispatch(addNode("New Node Name", "Parameter", "string", "This is a value", id));
+ControlsContainer.propTypes = {
+    handleAdd: func.isRequired,
+    handleConfirmRemove: func.isRequired,
+    handleConfirmEdit: func.isRequired,
+    onCancelEdit: func,
+    onBeginEdit: func
 }
 
-function handleConfirmRemove(ev, dispatch, id) {
-    console.log('removed ' + id);
-    ev.stopPropagation();
-    dispatch(removeNode(id));
+ControlsContainer.defaultProps = {
+    onBeginEdit: () => {},
+    onCancelEdit: () => {}
 }
 
-
-function handleEdit(ev, dispatch, id) {
-
-    console.log('edited ' + id);
-    ev.stopPropagation();
-}
-
-function mapDispatchToProps(dispatch, { visible, id }) {
+function mapDispatchToProps(dispatch, { visible, id, onCancelEdit, onBeginEdit }) {
     return {
-        handleAdd: ev => handleAdd(ev, dispatch, id),
-        handleConfirmRemove: ev => handleConfirmRemove(ev, dispatch, id),
-        handleEdit: ev => handleEdit(ev, dispatch, id),
-        visible
+        handleAdd: ev => {
+            console.log('added to ' + id);
+            ev.stopPropagation();
+            dispatch(addNode("New Node Name", "Parameter", "string", "This is a value", id));
+        },
+        handleConfirmRemove: ev => {
+            console.log('removed ' + id);
+            ev.stopPropagation();
+            dispatch(removeNode(id));
+        },
+        handleConfirmEdit: ev => {
+            console.log('edited ' + id);
+            ev.stopPropagation();
+        },
+        visible,
+        onCancelEdit,
+        onBeginEdit
     };
 }
 
---
+++ src/EditableNode.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { func } from 'prop-types';
+
+function EditableNode() {
+    return (
+        <div className="editable-tree-node">
+
+        </div>
+    );
+}
+
+export default EditableNode;
