813d47593746b27bd37c6edba72ec3c745d1ce66
Affan Shahid <affan.shahid@afiniti.com>
2017-09-02 18:33:29 +0500

    Add EditableNode styles

--- src/EditableNode.js
+++ src/EditableNode.js
@@ -1,21 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { func } from 'prop-types';
 import './EditableNode.css';
 
-function EditableNode() {
-    return (
-        <div className="editable-tree-node">
-            <textarea className="name" placeholder="Name"></textarea>
-            <textarea className="type" placeholder="Type"></textarea>
-            <textarea className="value"placeholder="Value"></textarea>
-            <select>
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-            </select>
-        </div>
-    );
+class EditableNode extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: '',
+            type: '',
+            value: '',
+            valueType: '0'
+        }
+
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handleTypeChange = this.handleTypeChange.bind(this);
+        this.handleValueChange = this.handleValueChange.bind(this);
+        this.handleValueTypeChange = this.handleValueTypeChange.bind(this);
+    }
+
+    handleNameChange(ev) {
+        this.setState({
+            name: ev.target.value
+        });
+
+        ev.stopPropagation();
+    }
+
+    handleTypeChange(ev) {
+        this.setState({
+            type: ev.target.value
+        });
+
+        ev.stopPropagation();
+    }
+
+    handleValueChange(ev) {
+        this.setState({
+            value: ev.target.value
+        });
+
+        ev.stopPropagation();
+    }
+
+    handleValueTypeChange(ev) {
+        this.setState({
+            valueType: ev.target.value
+        });
+
+        ev.stopPropagation();
+    }
+
+    render() {
+        return (
+            <div className="editable-tree-node">
+                <textarea onChange={this.handleNameChange} className="name" placeholder="Name" value={this.state.name} />
+                <select className="valuetype" value={this.state.valueType} onChange={this.handleValueTypeChange}>
+                    <option value="0" hidden>Value Type</option>
+                    <option>string</option>
+                    <option>integer</option>
+                    <option>double</option>
+                </select>
+                <textarea onChange={this.handleTypeChange} className="type" placeholder="Type" value={this.state.type}/>
+                <textarea onChange={this.handleValueChange} className="value" placeholder="Value" value={this.state.value}/>
+            </div>
+        );
+    }
 }
 
 export default EditableNode;
