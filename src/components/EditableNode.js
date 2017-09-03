495284c3b0291730f70de5a5e37b94f7a2bc27fd
Affan Shahid <affan.shahid.94@gmail.com>
2017-09-04 00:27:06 +0500

    Reorder editable fields

--- src/components/EditableNode.js
+++ src/components/EditableNode.js
@@ -57,13 +57,13 @@ class EditableNode extends Component {
         return (
             <div className="editable-tree-node">
                 <textarea onChange={this.handleNameChange} className="name" placeholder="Name" value={this.state.name} />
+                <textarea onChange={this.handleTypeChange} className="type" placeholder="Type" value={this.state.type} />
                 <select className="valuetype" value={this.state.valueType} onChange={this.handleValueTypeChange}>
                     <option value="0" hidden>Value Type</option>
                     <option>string</option>
                     <option>integer</option>
                     <option>double</option>
                 </select>
-                <textarea onChange={this.handleTypeChange} className="type" placeholder="Type" value={this.state.type} />
                 <textarea onChange={this.handleValueChange} className="value" placeholder="Value" value={this.state.value} />
                 <ControlsContainer 
                     disableOnClickOutside={true}
